ccd247ecfe7dbb0c4865daa542e03e8bac11a8f6
ranisharafeeque <ranisharafeeque@gmail.com>
2022-02-09 23:39:28 -0500

    updates

--- server/data/products.js
+++ server/data/products.js
@@ -9,7 +9,7 @@ const products = [
       countInStock: 15,
     },
     {
-      name: "Iphone 12",
+      name: "Iphone 13",
       imageUrl:
         "https://images.unsplash.com/photo-1605787020600-b9ebd5df1d07?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1463&q=80",
       description:
